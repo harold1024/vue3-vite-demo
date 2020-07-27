3ae5724f00b8c317c15acd82ac06c98de01dc0ad
harold1024 <744924498@qq.com>
2020-07-27 10:29:06 +0800

    demo

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <List />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import List from "./components/list.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    List,
+  },
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <div>{{ state.count1 }}</div>
+  <div>123</div>
+  <div>123</div>
+  <div>123</div>
+  <div>123</div>
+  <div>123</div>
+  <div>123</div>
+  <ul>
+    <li>123</li>
+  </ul>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  setup() {
+    const state = reactive({
+      count1: 1,
+    });
+    return { state };
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
